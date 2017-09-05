88895161de02c7a50d502b9d25e38c97b7b31036
lucas <lman817@gmail.com>
2017-09-05 14:28:56 -0400

    run through imgix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,145 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import moment from 'moment'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      </Router>
+    );
+  }
+}
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      publications: []
+    }
+  }
+
+  getSpeed() {
+    const query = new URLSearchParams(window.location.search)
+    return parseInt(query.get('speed') || -3600)
+  }
+
+  getTimestamp() {
+    const query = new URLSearchParams(window.location.search)
+    return parseInt(query.get('timestamp') || Math.round(Date.now() / 1000))
+  }
+
+  fetch() {
+    fetch(`https://pressminder.org/v1/publication?timestamp=${this.getTimestamp()}`)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        publications: data
+      })
+    })
+  }
+
+  componentWillMount() {
+    this.fetch()
+
+    this.interval = setInterval(() => {
+      const newTimestamp = this.getTimestamp() + this.getSpeed()
+      if (newTimestamp >= (Date.now() / 1000)) {
+        return
+      }
+      this.props.history.push(`?timestamp=${newTimestamp}`)
+    }, 5000)
+  }
+
+  componentWillReceiveProps() {
+    this.fetch()
+  }
+
+  render() {
+    const timestamp = this.getTimestamp()
+    return (
+      <div className="Home">
+        <div className="Home-header">
+          <DateSlider
+            value={timestamp}
+          />
+        </div>
+        {this.state.publications.map(publication => {
+          return (
+            <Publication
+              key={publication.id}
+              timestamp={timestamp}
+              publication={publication}
+            />
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+class Publication extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {articles: []}
+  }
+
+  fetch() {
+    fetch(`https://pressminder.org/v1/publication/${this.props.publication.id}/articles?count=5&timestamp=${this.props.timestamp}`)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        articles: data
+      })
+    })
+  }
+
+  componentWillMount() {
+    this.fetch()
+  }
+
+  componentWillReceiveProps() {
+    this.fetch()
+  }
+
+  render() {
+    return (
+      <div className="Publication">
+        <div className="Publication-preview">
+          <img src={`https://pressminder.imgix.net/${this.props.publication.screenshot}?fm=jpg&w=160`}/>
+        </div>
+        <div className="Publication-articles">
+          {this.state.articles.map(article => {
+            return (
+              <Article key={article.url} { ...article }/>
+            )
+          })}
+        </div>
       </div>
     );
   }
 }
+
+const Article = ({ title, url }) => (
+  <div className="Article">
+    <a className="Article-title" href={url}>
+      {title}
+    </a>
+  </div>
+)
+
+const DateSlider = ({ value }) => (
+  <div className="DateSlider">
+    <span className="DateSlider-value">{moment.unix(value).format('MMMM Do YYYY, h:mm a')}</span>
+  </div>
+)
 
 export default App;
