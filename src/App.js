dbede6c148a29413d85ce86d39a36aa705bfae15
lucas <lman817@gmail.com>
2017-11-01 13:49:52 -0400

    new bucket

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css'
 import FlipMove from 'react-flip-move'
+import Datetime from 'react-datetime'
 import {
   BrowserRouter as Router,
   Route  
 } from 'react-router-dom'
 
 async function fetchSnapshot(names, timestamp) {
-  return fetch(`https://pressminder.org/v1/snapshot/${names.join(',')}?count=8${timestamp ? '&timestamp=' + timestamp : ''}`)
+  return fetch(`https://pressminder.org/v1/snapshot/${names.join(',')}?count=10${timestamp ? '&timestamp=' + timestamp : ''}`)
   .then(response => response.json())
 }
 
@@ -27,31 +28,47 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      snapshot: {},
-      timestamp: null
+      snapshot: {}
     }
   }
 
-
-  componentWillMount() {
-    fetchSnapshot(['nyt'], this.state.timestamp)
+  fetch(timestamp) {
+    fetchSnapshot(['nyt', 'bbc'], timestamp)
     .then(snapshot => {
       this.setState({snapshot})
     })
   }
 
+
+  componentWillMount() {
+    this.fetch(null)
+  }
+
   render() {
     return (
       <div className="Home">
-        {Object.keys(this.state.snapshot).map(id => {
-          return (
-            <Publication
-              key={id}
-              articles={this.state.snapshot[id].articles}
-              screenshot={this.state.snapshot[id].screenshot}
+        <div className="Home-header">
+          <div className="Header-controls">
+            <Datetime
+              className="Header-datetime-control"
+              inputProps={{className: 'Header-datetime-control-input'}}
+              onChange={momentObj => {
+                this.fetch(momentObj.unix())
+              }}
             />
-          )
-        })}
+          </div>
+        </div>
+        <div className="Home-content">
+          {Object.keys(this.state.snapshot).map(id => {
+            return (
+              <Publication
+                key={id}
+                articles={this.state.snapshot[id].articles}
+                screenshot={this.state.snapshot[id].screenshot}
+              />
+            )
+          })}
+        </div>
       </div>
     )
   }
@@ -63,7 +80,7 @@ class Publication extends Component {
     return (
       <div className="Publication">
         <div className="Publication-screenshot">
-          <img alt="screenshot" src={`https://s3.amazonaws.com/pressminder/${this.props.screenshot}`}/>
+          <img alt="screenshot" src={`https://d1qd36z8dssjk5.cloudfront.net/${this.props.screenshot}`}/>
         </div>
         <FlipMove
           enterAnimation="none" 
